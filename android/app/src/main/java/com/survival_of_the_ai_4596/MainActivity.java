33f7676ff27219b559ced7077ba0bb08a30ac11d
Crowdbotics <team@crowdbotics.com>
2019-06-13 02:52:42 +0000

    initial commit

---
+++ android/app/src/main/java/com/survival_of_the_ai_4596/MainActivity.java
@@ -0,0 +1,15 @@
+package com.survival_of_the_ai_4596;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "survival_of_the_ai_4596";
+    }
+}
